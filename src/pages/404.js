13939e0df066f6493424c56cb0601ab6d04889d0
sagiv.bengiat <sagiv.bengiat@appsflyer.com>
2019-10-27 09:21:45 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { graphql } from "gatsby"
-
+import { graphql, Link } from "gatsby"
+import { rhythm } from "../utils/typography"
+import ReadingTime from '../components/ReadingTime'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,12 +9,34 @@ class NotFoundPage extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges;
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>4🙄4 - Not Found</h1>
+        <p>This post doesn&#39;t exist... Yet.</p>
+        <h3>These are the latest stories </h3>
+        {
+          posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <article key={node.fields.slug} style={{marginBottom: '15px'}}>
+                <header>
+                  <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                    {title}
+                  </Link>
+                  <span>
+                    {` - `}
+                    <small>
+                      {node.frontmatter.date}
+                    </small>
+                  </span>
+                </header>
+              </article>
+            )
+          })
+        }
       </Layout>
     )
   }
@@ -26,6 +49,21 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          timeToRead
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
       }
     }
   }
